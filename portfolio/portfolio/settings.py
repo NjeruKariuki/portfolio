8be5af03c19b63d4a99d3708c954bef45cae43b4
DanielNjeru <58853234+NjeruKariuki@users.noreply.github.com>
2022-03-02 09:35:19 +0300

    pass change

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -135,6 +135,6 @@ django_heroku.settings(locals())
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'freakoutbond2@gmail.com'
-EMAIL_HOST_PASSWORD = '0703888630'
+EMAIL_HOST_PASSWORD = 'Goodkidgonebad87'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
